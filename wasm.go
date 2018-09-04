9d8326ac6f33edefd89f0b75cc6909c6b8c72cc5
George Armhold <armhold@gmail.com>
2018-09-04 09:08:13 -0400

    update the DOM via timer

--- wasm.go
+++ wasm.go
@@ -1,6 +1,11 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"syscall/js"
+	"time"
+)
 
 // to compile:
 //
@@ -8,4 +13,25 @@ import "log"
 //
 func main() {
 	log.Printf("hello world!")
+
+	tickChan := time.NewTicker(time.Millisecond * 1000).C
+
+	for {
+		select {
+
+		case <-tickChan:
+			log.Println("Ticker ticked")
+
+			t := time.Now()
+			ts := t.Format("Mon Jan _2 15:04:05 2006")
+
+			s := fmt.Sprintf("<p>Time is: %v</p>", ts)
+			updateHTML(s)
+		}
+	}
+}
+
+func updateHTML(s string) {
+	el := js.Global().Get("document").Call("getElementById", "foo")
+	el.Set("innerHTML", s)
 }
